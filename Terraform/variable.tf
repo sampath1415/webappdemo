c1c56549a140a8edffcb6edb3ea9f33b44ccb859
sampath1415 <sampath1415@gmail.com>
2021-03-31 16:20:12 +0530

    Create variable.tf

---
+++ Terraform/variable.tf
@@ -0,0 +1,13 @@
+variable "vnet_address_prefix" {
+  type="string"
+  default= ["10.0.0./16"]
+  
+  }
+
+varibale "subnet_address_prefix"{
+  type = "string"
+  default= ["10.0.1.0/24"]
+  
+  }
+  
+  
