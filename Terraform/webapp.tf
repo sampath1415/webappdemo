3b1e7a97e0165eec2a6cd66636bef36e81606cc0
sampath1415 <sampath1415@gmail.com>
2021-03-31 15:47:11 +0530

    Update webapp.tf

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -38,7 +38,7 @@ resource "azurerm_subnet" "integrationsubnet" {
   name                 = "integrationsubnet"
   resource_group_name  = azurerm_resource_group.dev.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefix     = ["10.0.1.0/24"]
   delegation {
     name = "delegation"
     service_delegation {
