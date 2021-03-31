34ace8f06f5068bada7767bdcaa2f168ccec0aab
sampath1415 <sampath1415@gmail.com>
2021-03-31 18:43:58 +0530

    Update variable.tf

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -3,8 +3,8 @@ variable "vnet_address_prefix" {
     }
 
 variable "subnet_address_prefix"{
-    type=list(string)
-    default= ["10.0.1.0/24"]
+    type= string
+    default= "10.0.1.0/24"
     }
   
   
